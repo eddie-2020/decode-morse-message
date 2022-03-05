05cbebf3bac771f696f2a7ff3f2989dc73385457
Edward <oluyaratosin123@gmail.com>
2022-03-05 01:57:44 +0100

    Fix linters offences

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,34 +1,31 @@
 def decode_morse_char(code)
-    decode_morser = { 
-        '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', 
-        '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', 
-        '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', 
-        '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', 
-        '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', 
-        '--..' => 'Z', '.----' => '1', '..---' => '2', '...--' => '3', '....-' => '4', 
-        '.....' => '5', '-....' => '6', '--...' => '7', '---..' => '8', '----.' => '9', 
-        '-----' => '0' }
-    decode_morser[code]
+  decode_morser = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+                    '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+                    '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+                    '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z', '.----' => '1',
+                    '..---' => '2', '...--' => '3', '....-' => '4', '.....' => '5', '-....' => '6', '--...' => '7',
+                    '---..' => '8', '----.' => '9', '-----' => '0' }
+  decode_morser[code]
 end
 
 puts decode_morse_char('.-')
 
 def decode_morse_word(code)
-    result = ''
-    arr = code.split
-    arr.each do |char|
-        result += decode_morse_char(char)
-    end
-    result
+  result = ''
+  arr = code.split
+  arr.each do |char|
+    result += decode_morse_char(char)
+  end
+  result
 end
 
 puts decode_morse_word('-- -.--')
 
 def decode_morse(code)
-    result = ''
-    arr = code.split('  ')
-    arr.each { |item| result += "#{decode_morse_word(item)}" }
-    result.strip
+  result = ''
+  arr = code.split('  ')
+  arr.each { |item| result += decode_morse_word(item).to_s }
+  result.strip
 end
 
 puts decode_morse('-- -.--  -. .- -- .')
